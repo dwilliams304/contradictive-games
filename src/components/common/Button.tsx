6960ac27c7d3100beaa4ab57073e9f5837650b34
dwilliams304 <d.williams0434@gmail.com>
2024-12-08 18:35:35 -0500

    add button component, allow explicist any for ts

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,24 @@
+type ButtonProps = {
+    classes: string;
+    styleType?: "primary" | "secondary" | "tertiary";
+    type: "button" | "submit" | "reset";
+    fn: ((...args: any[]) => void);
+    text?: string;
+    tooltip?: string;
+    disabled: boolean;
+}
+
+export default function Button(props: ButtonProps){
+    const {classes, styleType, type, fn, text, tooltip, disabled} = props;
+
+
+    return(
+        <button className={`btn-${styleType} ${classes}`}
+        title={tooltip}
+        type={type}
+        disabled={disabled}
+        onClick={fn}>
+            {text}
+        </button>
+    )
+}
